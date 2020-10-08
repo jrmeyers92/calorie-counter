2c26a6af45762d0dc77e82c57ce2ead12923c49b
Jake Meyers <jakejake92@yahoo.com>
2020-10-07 19:43:31 -0600

    api call in progress

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,13 +3,22 @@ import React, { useState } from "react";
 const Input = () => {
 	const [searchTerm, setSearchTerm] = useState(" ");
 
+	const getNutrition = async () => {
+		const response = await fetch(
+			`https://api.edamam.com/api/food-database/v2/nutrients?app_id=${"4b30167a"}&app_key=${"a63e746fe6fe1ea55aeb0f0cfeec9022"}`
+		);
+		const data = response.json();
+		console.log(data);
+	};
+
 	return (
 		<div className='ui fluid input'>
 			<input
 				type='text'
 				placeholder='search your food'
-				onChange={(e) => setSearchTerm(e.target.value)}
-				value={searchTerm}
+				// onChange={(e) => setSearchTerm(e.target.value)}
+				// value={searchTerm}
+				onChange={getNutrition}
 			/>
 		</div>
 	);
