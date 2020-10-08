7194c12dce39ee311a5a7ad36b6d062ccdd1ed11
Jake Meyers <jakejake92@yahoo.com>
2020-10-08 16:54:58 -0600

    api working

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,7 +5,7 @@ const Input = () => {
 
 	const getNutrition = async () => {
 		const response = await fetch(
-			`https://api.edamam.com/api/food-database/v2/parser?nutrition-type=logging&ingr=red%20apple&app_id=${"4b30167a"}&app_key=${"a63e746fe6fe1ea55aeb0f0cfeec9022"}`
+			`https://api.edamam.com/api/food-database/v2/parser?nutrition-type=logging&ingr=${searchTerm}&app_id=${"4b30167a"}&app_key=${"a63e746fe6fe1ea55aeb0f0cfeec9022"}`
 		);
 		const data = response.json();
 		console.log(data);
@@ -16,12 +16,24 @@ const Input = () => {
 			<input
 				type='text'
 				placeholder='search your food'
-				// onChange={(e) => setSearchTerm(e.target.value)}
-				// value={searchTerm}
-				onChange={getNutrition}
+				onChange={(e) => {
+					setSearchTerm(e.target.value);
+					getNutrition({ searchTerm });
+				}}
 			/>
 		</div>
 	);
+
+	// <div className='ui fluid input'>
+	// 	<input
+	// 		type='text'
+	// 		placeholder='search your food'
+	// 		onChange={(e) => {
+	// 			setSearchTerm(e.target.value)
+	// 			value={searchTerm}
+	// 			getNutrition({searchTerm});
+	// 		/>
+	// </div>
 };
 
 export default Input;
